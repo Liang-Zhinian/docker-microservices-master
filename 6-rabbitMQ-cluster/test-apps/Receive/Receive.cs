9e82e413d6da76b298fdce928453f1ae9cf908fe
Liang Zhinian <a13533550310@live.com>
2020-07-26 10:25:14 +0800

    add rabbitmq-cluster test apps

---
+++ 6-rabbitMQ-cluster/test-apps/Receive/Receive.cs
@@ -0,0 +1,78 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+/**
+If you want to save only 'warning' and 'error' (and not 'info') log messages to a file, just open a console and type:
+
+dotnet run warning error > logs_from_rabbit.log
+If you'd like to see all the log messages on your screen, open a new terminal and do:
+
+dotnet run info warning error
+# => [*] Waiting for logs. To exit press CTRL+C
+
+*/
+class Receive
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "192.168.99.107", Port = 5675 };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            string queueName = "q.200304";
+            string exchange = "direct_logs";
+
+            // receive message from exchange
+            channel.ExchangeDeclare(exchange: exchange,
+                type: "direct",
+                durable: true // persistence
+                );
+            queueName = channel.QueueDeclare().QueueName;
+            
+            if (args.Length < 1) 
+            {
+                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                        Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+            
+            foreach(var severity in args)
+            {
+                channel.QueueBind(queue: queueName,
+                    exchange: exchange,
+                    routingKey: severity);
+            }
+
+            // receive message from queue
+            // channel.QueueDeclare(queue: queueName, 
+            //     durable: false, 
+            //     exclusive: false, 
+            //     autoDelete: false, 
+            //     arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                  routingKey, message);
+            };
+            channel.BasicConsume(queue: queueName, 
+                autoAck: true, 
+                consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6-rabbitMQ-cluster/test-apps/Send/Send.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+
+/**
+dotnet run error "Run. Run. Or it will explode."
+*/
+class Send
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "192.168.99.107", Port = 5675 };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            string queueName = "q.200304";
+            string exchange = "direct_logs";
+
+            var severity = (args.Length > 0) ? args[0]: "info";
+
+            string message = (args.Length > 1)
+                                ? string.Join(" ", args.Skip(1).ToArray())
+                                : "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.ExchangeDeclare(
+                exchange: exchange,
+                type: "direct",
+                durable: true // persistence
+            );
+
+            //定义  队列 队列名<queueName>,持久化的,非排它的,非自动删除的。
+            queueName = channel.QueueDeclare(queueName, 
+                                true, 
+                                false, 
+                                false, 
+                                null).QueueName;
+            channel.QueueBind(queueName, exchange, severity);//队列绑定交换器
+
+
+            //生产者回调函数
+            channel.BasicReturn += (model, ea) =>
+            {
+                //do something... 消息若不能路由到队列则会调用此回调函数。
+                
+                var b = ea.Body;
+                var msg = Encoding.UTF8.GetString(b);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Message returns '{0}':'{1}'",
+                                  routingKey, msg);
+            };
+
+            // send message to exchange
+            channel.BasicPublish(exchange: exchange, 
+                routingKey: severity, 
+                mandatory: true,
+                basicProperties: null, 
+                body: body);
+
+            // 发布一个不可以路由到队列的消息，mandatory参数设置为true
+            var message1 = Encoding.UTF8.GetBytes("TestMsg1");
+            channel.BasicPublish(exchange, "routingKey1", true, null, message1);
+
+            
+            // send message to queue
+            // channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            // channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            
+            Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
